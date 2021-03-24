27b6f0ead26dcd78ca4586be2dd9aa9693e3bf19
ShortFir <shortfir@protonmail.com>
2021-03-24 16:16:09 +1100

    Delete button active. Doesn't delete an opeartor though.

--- js/script.js
+++ js/script.js
@@ -46,108 +46,93 @@ function updateDisplay()
 {
     inputDisplay.textContent = displayString;
 }
+function operateButton(buttonId)
+{
+    (runningTotal === 0) ?
+        runningTotal = parseInt(currentValue) :
+        runningTotal = operate(runningTotal, parseInt(currentValue), operator);
+    operator = buttonId; // placed after operation to avoid logic errors
+    currentValue = "";
+}
 function buttonClick(buttonId)
 {
     switch(buttonId)
     {
         case "zeroButton":
-            currentValue += "0";
-            displayString += "0";
-            // inputDisplay.textContent = displayString;
-            updateDisplay()
+            if(currentValue !== "")
+            {
+                currentValue += "0";
+                displayString += "0";
+                updateDisplay()
+            }else
+            {
+                outputDisplay.textContent += "Error! No leading zeros." + `\n`;
+                outputDisplay.scrollTop = outputDisplay.scrollHeight;
+            }
             break;
         case "oneButton":
             currentValue += "1";
             displayString += "1";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "twoButton":
             currentValue += "2";
             displayString += "2";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "threeButton":
             currentValue += "3";
             displayString += "3";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "fourButton":
             currentValue += "4";
             displayString += "4";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "fiveButton":
             currentValue += "5";
             displayString += "5";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "sixButton":
             currentValue += "6";
             displayString += "6";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "sevenButton":
             currentValue += "7";
             displayString += "7";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "eightButton":
             currentValue += "8";
             displayString += "8";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "nineButton":
             currentValue += "9";
             displayString += "9";
-            // inputDisplay.textContent = displayString;
             updateDisplay()
             break;
         case "divideButton":
-            (runningTotal === 0) ?
-                runningTotal = parseInt(currentValue) :
-                runningTotal = operate(runningTotal, parseInt(currentValue), operator);
-            operator = buttonId; // placed after operation to avoid logic errors
-            currentValue = "";
-            displayString += `\u00F7`;
-            // inputDisplay.textContent = displayString;
+            displayString += `\u00F7`; // Divide symbol
+            operateButton(buttonId)
             updateDisplay()
             break;
         case "multiplyButton":
-            (runningTotal === 0) ?
-                runningTotal = parseInt(currentValue) :
-                runningTotal = operate(runningTotal, parseInt(currentValue), operator);
-            operator = buttonId; // placed after operation to avoid logic errors
-            currentValue = "";
             displayString += "x";
-            // inputDisplay.textContent = displayString;
+            operateButton(buttonId)
             updateDisplay()
             break;
         case "subtractButton":
-            (runningTotal === 0) ?
-                runningTotal = parseInt(currentValue) :
-                runningTotal = operate(runningTotal, parseInt(currentValue), operator);
-            operator = buttonId; // placed after operation to avoid logic errors
-            currentValue = "";
             displayString += "-";
-            // inputDisplay.textContent = displayString;
+            operateButton(buttonId)
             updateDisplay()
             break;
         case "addButton":
-            (runningTotal === 0) ?
-                runningTotal = parseInt(currentValue) :
-                runningTotal = operate(runningTotal, parseInt(currentValue), operator);
-            operator = buttonId; // placed after operation to avoid logic errors
-            currentValue = "";
             displayString += "+";
-            // inputDisplay.textContent = displayString;
+            operateButton(buttonId)
             updateDisplay()
             break;
         case "clearButton":
@@ -158,18 +143,21 @@ function buttonClick(buttonId)
             // {
             //     operator = "";
             //     displayString = displayString.split("").slice(0, -1).join("");
-            // }else
-            // {
-            //     currentValue = currentValue.split("").slice(0, -1).join("");
-            //     displayString = displayString.split("").slice(0, -1).join("");
             // }
-            // inputDisplay.textContent = displayString;
+            if(currentValue !== "")
+            {
+                currentValue = currentValue.split("").slice(0, -1).join("");
+                displayString = displayString.split("").slice(0, -1).join("");
+                updateDisplay();
+            }
             break;
         case "equalButton":
             if(runningTotal !== 0 && currentValue !== "")
             {
                 runningTotal = operate(runningTotal, parseInt(currentValue), operator);
-                outputDisplay.textContent += displayString + " = " + runningTotal + `\n`;
+                outputDisplay.textContent += displayString + " = " +
+                    ((runningTotal % 1 !== 0) ? runningTotal.toFixed(2) : runningTotal) +
+                    `\n`;
                 outputDisplay.scrollTop = outputDisplay.scrollHeight;
                 clearInputDisplay();
             }
